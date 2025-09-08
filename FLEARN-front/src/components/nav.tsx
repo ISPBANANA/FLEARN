7c197e259e27e6143768e74843bf87a47f948828
Techaphatr INDHAVIVADHANA <techaphatr.i@ku.th>
2025-09-08 15:31:39 +0700

    Add ProfileIcon and Setup NAV for whole website

---
+++ FLEARN-front/src/components/nav.tsx
@@ -0,0 +1,44 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+export function Nav() {
+    const pathname = usePathname();
+
+    const navItems = [
+        { name: 'Home', path: '/' },
+        { name: 'Learn', path: '/learn' },
+        { name: 'About Us', path: '/about' },
+    ];
+
+    return (
+        <nav className="flex justify-between items-center p-4 w-full">
+            <p className="text-[#9A41FF] text-2xl font-bold"><Link href="/">FLearn</Link></p>
+            <ul className="flex gap-8 items-center">
+                {navItems.map((item) => (
+                    <li key={item.name}>
+                        <Link
+                            href={item.path}
+                            className={`text-[#9A41FF] transition-all duration-200
+                                ${pathname === item.path
+                                    ? 'opacity-100 border-b-2 border-[#9A41FF] pb-1'
+                                    : 'opacity-70 hover:opacity-100'
+                                }`}
+                        >
+                            {item.name}
+                        </Link>
+                    </li>
+                ))}
+                <li>
+                    <Image
+                        src="/profile-icon.svg"
+                        alt="ProfileIcon"
+                        width={32}
+                        height={32}
+                        className="text-[#9A41FF]"
+                    />
+                </li>
+            </ul>
+        </nav>
+    );
+}
