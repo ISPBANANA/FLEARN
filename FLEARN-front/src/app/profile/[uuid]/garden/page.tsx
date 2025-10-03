c734b14d7246a3a36122f8e27bfe1bc1b04b6f4b
Techaphatr INDHAVIVADHANA <teetartube@gmail.com>
2025-10-03 19:35:18 +0700

    setup static page for garden page

--- FLEARN-front/src/app/profile/[uuid]/garden/page.tsx
+++ FLEARN-front/src/app/profile/[uuid]/garden/page.tsx
@@ -11,6 +11,7 @@ import { useAPIWithCORSHandling } from '@/hooks/useCORS';
 import { CORSErrorDisplay } from '@/components/CORSErrorHandler';
 import { useAuth } from "@/hooks/useAuth";
 import { userAPI } from '@/lib/api';
+import { UserRoundPlus } from 'lucide-react';
 
 // Force dynamic rendering to avoid build-time issues with useSearchParams
 export const dynamic = 'force-dynamic';
@@ -101,21 +102,56 @@ export default function garden({ params }: ProfilePageProps) {
       <div className="min-h-screen bg-white">
         <Nav />
         {/* Infomation here */}
-        <div className="my-2 p-4 h-auto w-full flex items-center z-1 bg-white flex-col min-h-screen max-w-[1620px]">
-          <h1 className="text-5xl font-bold mb-4 text-[#9A41FF]">{profileData?.name}&rsquo;s Garden</h1>
+        <div className="my-2 p-4 h-auto w-full flex items-center justify-center align-center z-1 bg-white flex-col min-h-screen">
+          <div className="w-full max-w-[1620px] items-center flex flex-col px-25 py-7">
+            <p className="text-5xl font-bold mb-4 text-[#9A41FF] text-center w-full">{profileData?.name}&rsquo;s Garden</p>
 
-          <div className="h-4/5 min-h-[calc(75vh)] grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 w-full max-w-[1420px] px-24 py-4">
-            <div>
-              <h1 className="text-3xl font-bold mb-4">Garden</h1>
+            {/* Garden List */}
+            <ul className="h-4/5 min-h-[calc(75vh)] flex flex-row flex-wrap justify-center gap-6 w-full max-w-[1420px] px-24 py-4 place-items-start auto-rows-max">
+              {/* Template */}
+              <li className="w-full max-w-[300px] min-w-[300px] min-h-[370px] rounded-lg flex flex-col gap-4 items-center p-5 justify-between" style={{boxShadow: '0px 0px 5px rgba(0, 0, 0, 0.25)' }}>
+                <p className="text-2xl font-semibold mb-4 text-center text-[#454545]">Name</p>
+                <div className="flex flex-col items-center gap-2">
+                  <Image
+                    src="/garden/plant1.png"
+                    alt="Plant 1"
+                    width={140}
+                    height={140}
+                  />
+                  <div className="items-center align-center justify-center flex flex-row gap-4">
+                    <Image
+                      src="/stopSteak.png"
+                      alt="Plant 1"
+                      width={35}
+                      height={35}
+                    />
+                    <p className="text-center text-lg text-[#454545] h-max">Steak: NA</p>
+                  </div>
+                </div>
+              </li>
+
+              {/* Add/Invite/Accept/Request */}
+              <li className="w-full max-w-[300px] min-w-[300px] min-h-[370px] rounded-lg flex flex-col gap-4 items-center p-5 justify-center border border-dashed border-2 border-[#9A41FF]">
+                <button className="text-[#454545] cursor-pointer hover:scale-105 transition flex flex-col items-center">
+                  <div className="flex flex-col items-center gap-2">
+                    <UserRoundPlus 
+                      height={50}
+                      width={50}
+                    />
+                    <p className="text-center text-lg text-[#454545] h-max">Invite Friend</p>
+                  </div>
+                </button>
+              </li>
+
+            </ul>
+            <div className="h-1/5 my-10 justify-start flex w-full max-w-[1420px] px-24">
+              <button
+                onClick={() => profileData?.user_id && router.push(`/profile/${profileData.user_id}`)}
+                className={`bg-[#ffffff] text-[#454545] border border-[#454545] py-2 px-4 w-50 rounded transition h-10 cursor-pointer`}
+              >
+                To Profile
+              </button>
             </div>
-          </div>
-          <div className="h-1/5 my-10 justify-start flex w-full max-w-[1420px] px-24">
-            <button
-              onClick={() => profileData?.user_id && router.push(`/profile/${profileData.user_id}`)}
-              className={`bg-[#ffffff] text-[#454545] border border-[#454545] py-2 px-4 w-50 rounded transition h-10 cursor-pointer`}
-            >
-              To Profile
-            </button>
           </div>
           <div className="py-5"></div>
         </div>
