1edb40849ede3f6d66274d612043eeadf1e8bb22
Sethtatad Kijkanjanarat <sethatatad.k@ku.th>
2025-11-08 00:04:50 +0700

    make sharing responsive

--- FLEARN-front/src/app/policy/page.tsx
+++ FLEARN-front/src/app/policy/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
             </p>
           </div>
 
-          <div className="text-lg text-start text-[#454545] mb-4 py-3 w-full">
+          <div className="text-base sm:text-lg text-left text-[#454545] py-6 w-full leading-relaxed">
             <b style={{ fontWeight:550 }}>Sharing of Information:</b> <br></br>
             We respect your privacy and do not sell your personal information to third parties. 
             We may share information only under limited circumstances: <br></br>
