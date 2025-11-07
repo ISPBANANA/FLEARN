9b3dff08e74b7d113b8ba37ad1479fb8dbab25be
Sethtatad Kijkanjanarat <sethatatad.k@ku.th>
2025-11-07 22:24:12 +0700

    Make topic 11 responsive

--- FLEARN-front/src/app/tos/page.tsx
+++ FLEARN-front/src/app/tos/page.tsx
@@ -139,11 +139,11 @@ export default function Home() {
         </div>
 
         {/* no.11 */}
-        <div className="text-lg text-start text-[#454545] mb-4 py-2 w-full">
+        <div className="text-base sm:text-lg text-left text-[#454545] py-6 w-full leading-relaxed">
           <h2 className="text-lg sm:text-xl font-semibold mb-2">
             11&#41; Beta features & changes:
           </h2>
-            <p className="px-5">
+            <p className="px-5 sm:px-5">
               We may test new or experimental features (“Beta”).  
               Beta is provided as-is and may change or stop at any time. 
               We may also update lessons (e.g., to fix errors or improve alignment).<br></br>
