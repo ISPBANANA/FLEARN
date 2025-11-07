45de92e8392d6c44db12b8ea670cd8dba5cb8427
Sethtatad Kijkanjanarat <sethatatad.k@ku.th>
2025-11-07 22:21:04 +0700

    Make topic 5 responsive

--- FLEARN-front/src/app/tos/page.tsx
+++ FLEARN-front/src/app/tos/page.tsx
@@ -64,11 +64,11 @@ export default function Home() {
         </div>
 
         {/* no.5 */}
-        <div className="text-lg text-start text-[#454545] mb-4 py-2 w-full">
+        <div className="text-base sm:text-lg text-left text-[#454545] py-6 w-full leading-relaxed"> 
           <h2 className="text-lg sm:text-xl font-semibold mb-2">
             5&#41; Your content:
           </h2>         
-          <p className="px-5">
+          <p className="px-5 sm:px-5">
             If you submit text, answers, notes, images, or feedback (“User Content”), you grant us a worldwide, non-exclusive, 
             royalty-free license to use, reproduce, modify, and display that content only to operate and improve the Service. 
             You retain ownership of your User Content.<br></br>
