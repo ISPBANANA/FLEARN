467979a9138b456f8064ff02473bf367fd3eac20
Sethtatad Kijkanjanarat <sethatatad.k@ku.th>
2025-11-07 22:27:11 +0700

    Make contact responsive

--- FLEARN-front/src/app/tos/page.tsx
+++ FLEARN-front/src/app/tos/page.tsx
@@ -234,7 +234,7 @@ export default function Home() {
         </div>
 
         {/* Contact */}
-        <div className="text-lg text-start text-[#454545] mb-4 py-15 w-full">
+        <div className="text-base sm:text-lg text-left text-[#454545] py-6 w-full leading-relaxed">
             Ispbanana (Flearn Team)<br></br>
             ■ Email: ispbanana.contact@gmail.com<br></br>
             ■ Website: hongrocker49.thddns.net:2725
