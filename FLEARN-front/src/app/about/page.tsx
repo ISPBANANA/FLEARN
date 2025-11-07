1b79b698d13ff10853b7a39235d76563cedd0d86
Sethtatad Kijkanjanarat <sethatatad.k@ku.th>
2025-11-08 00:42:50 +0700

    make About us topic responsive

--- FLEARN-front/src/app/about/page.tsx
+++ FLEARN-front/src/app/about/page.tsx
@@ -7,9 +7,9 @@ export default function Home() {
       <Nav />
 
       {/* Infomation here */}
-      <div className="my-2 p-4 h-auto w-full flex items-center z-1 bg-white flex-col min-h-screen">
-        <div className="w-full max-w-[1620px] items-center flex flex-col px-25 py-7">
-          <p className="text-5xl text-center text-[#9A41FF] font-bold">
+      <div className="my-2 p-4 h-auto w-full flex items-center z-10 bg-white flex-col min-h-screen">
+        <div className="w-full max-w-none mx-auto items-center flex flex-col px-6 sm:px-8 md:px-12 lg:px-24 py-7">
+          <p className="text-3xl sm:text-4xl md:text-5xl text-center text-[#9A41FF] font-bold leading-tight">
             About us
           </p>
 
