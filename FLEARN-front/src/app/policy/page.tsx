aa6e4298a95a8673a497419d5ae07e93a3ada8e3
Sethtatad Kijkanjanarat <sethatatad.k@ku.th>
2025-11-07 23:54:25 +0700

    remove unused import from privacy

--- FLEARN-front/src/app/policy/page.tsx
+++ FLEARN-front/src/app/policy/page.tsx
@@ -1,7 +1,5 @@
-import Image from "next/image";
 import { Nav } from "@/components/nav";
 import { Footer } from "@/components/footer";
-import SplitText from "@/components/SplitText";
 
 export default function Home() {
   return (
